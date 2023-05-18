4d2ec6928504ffe580589dee022d4b8423645f8e
Dumisani Tom <s212317165@gmail.com>
2023-05-18 14:14:49 +0200

    // working on using animation instead of setint

--- Day 1/src/Landing.tsx
+++ Day 1/src/Landing.tsx
@@ -1,3 +1,4 @@
+import { useState, useRef, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faEgg,
@@ -11,10 +12,9 @@ import {
   faCookie,
   faSeedling,
 } from "@fortawesome/free-solid-svg-icons";
-import "./landing.css";
+import "./landing.scss";
 const icons: any = [
   faEgg,
-  faStroopwafel,
   faCheese,
   faHotdog,
   faDrumstickBite,
@@ -22,16 +22,55 @@ const icons: any = [
   faIceCream,
   faFish,
   faCookie,
+  faStroopwafel,
   faSeedling,
 ];
+
 const Landing = () => {
+  const [classNumber, setClassnumber] = useState<number>(0);
+  const refa = useRef(new Array(10));
+  const slider_animation = () => {
+    if (classNumber > icons.length - 1) {
+      setClassnumber((prev: number) => prev - prev);
+    } else {
+      console.log(classNumber);
+      setClassnumber((prev: number) => prev + 1);
+    }
+  };
+  useEffect(() => {
+    /* const interval = setInterval(() => {
+      setClassnumber((prev: number) => prev + 1);
+    }, 2000);
+  
+    return () => clearInterval(interval);*/
+    console.log(refa.current.length);
+    refa.current[classNumber] = requestAnimationFrame(slider_animation);
+
+    return () => cancelAnimationFrame(refa.current[classNumber]);
+  }, []);
+  /*  useEffect(() => {
+    const time = 2000 * icons.length;
+    const interval = setInterval(() => {
+      setClassnumber((prev: number) => prev - prev);
+    }, time);
+
+    return () => clearInterval(interval);
+  }, []);*/
+  console.log(classNumber);
   return (
     <div className="landing">
       <h1 className="landing_heading">Variety of Foods</h1>
 
-      <div className="landing_container_icons">
+      <div>
         {icons.map((icon: any, index: number) => (
-          <FontAwesomeIcon className="landing_icon" key={index} icon={icon} />
+          <FontAwesomeIcon
+            ref={(element) => refa.current.push(element)}
+            className={
+              "landing_icon " + (classNumber == index ? "active_icon" : "")
+            }
+            key={index}
+            icon={icon}
+          />
         ))}
       </div>
     </div>
