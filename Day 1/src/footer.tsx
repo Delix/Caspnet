08a87f2847a0c78f29bf7f900c9d42642bcab580
Dumisani Tom <s212317165@gmail.com>
2023-05-23 18:40:21 +0200

    90

---
+++ Day 1/src/footer.tsx
@@ -0,0 +1,23 @@
+import "./footer.scss";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const footer = () => {
+  return (
+    <div className="Footer">
+      <div className="footer_text_container">
+        <h2 className="footer_heading">Sign Me up</h2>
+        <div>
+          <p className="footer_text">Be the first to know about new products</p>
+        </div>
+      </div>
+      <div className="footer_input_Container">
+        <input placeholder="Enter Your Email" type="text" />
+        <FontAwesomeIcon icon={faArrowRight} className="input_icon" />
+      </div>
+      <p className="copyright">Delix&copy; All right Reserved</p>
+    </div>
+  );
+};
+
+export default footer;
