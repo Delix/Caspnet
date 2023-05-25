2e2b539cc455994f2e16dad80572ef091b25deb0
Dumisani Tom <s212317165@gmail.com>
2023-05-25 20:23:08 +0200

    99

--- Day 1/src/nav.tsx
+++ Day 1/src/nav.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import {
   faHome,
   faUtensils,
@@ -10,10 +11,30 @@ import "./nav.scss";
 const icons = [faHome, faUtensils, faBurger, faPizzaSlice, faBlenderPhone];
 const titles = ["Home", "Meals", "Burger", "Pizza", "Contact"];
 const nav = () => {
+  const changeview = useRef();
+  const anchor = useRef();
+  const changeopacity = useRef([]);
+  const changeStyle = () => {
+    anchor.current.classList.toggle("change");
+    changeview.current.classList.toggle("change");
+    changeopacity.current.forEach((element) => {
+      element.classList.toggle("change");
+      console.log(element);
+    });
+  };
+  console.log(changeopacity);
   return (
-    <nav className="Navbar">
+    <nav ref={changeview} className="Navbar">
+      <div ref={anchor} className="menu">
+        <div className="menu_tail" />
+        <div onClick={changeStyle} className="menu_head" />
+      </div>
       {icons.map((icon, index) => (
-        <a className="navbar_link" key={index}>
+        <a
+          ref={(e) => changeopacity.current.push(e)}
+          className="navbar_link"
+          key={index}
+        >
           <FontAwesomeIcon icon={icon} className="navbar_link_icon" />
           <span className="navbar_link_text">{titles[index]}</span>
         </a>
